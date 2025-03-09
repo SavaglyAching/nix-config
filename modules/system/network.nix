4567e22be7ab8bdc0a7cfba0b11069620a0eba75
Macr <macr@mail.com>
2025-03-09 08:50:02 -0300

    trying to fix rebuilds

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -2,22 +2,13 @@
 
 {
   networking = {
-    # Hostname is set in the host-specific configuration
-
-    
-    # Firewall settings
+ 
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 ];
       allowedUDPPorts = [ 41641 ]; # Tailscale port
       allowPing = false;
       trustedInterfaces = [ "tailscale0" ];
-      
-            # Extra rules for remote builder
-      extraCommands = ''
-        # Allow all connections from the Surface device for Nix remote building
-        iptables -A INPUT -p tcp -s 192.168.2.0/24 --dport 22 -j ACCEPT
-      '';
     };
     
 
