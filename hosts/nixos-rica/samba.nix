1508c728c2c41e39028618f4d56fb41219eeef70
SavaglyAching <154733755+SavaglyAching@users.noreply.github.com>
2025-03-10 18:05:55 -0300

    Update samba.nix

--- hosts/nixos-rica/samba.nix
+++ hosts/nixos-rica/samba.nix
@@ -23,6 +23,18 @@
         "force user" = "ham";
         "valid users" = "ham";
       };
+      # New share for /home/ham
+      home = {
+        path = "/home/ham";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0664";
+        "directory mask" = "0775";
+        "force user" = "ham";
+        "valid users" = "ham";
+        "comment" = "Home directory for ham user";
+      };
     };
   };
 }
