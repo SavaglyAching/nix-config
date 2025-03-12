ebd21124cdd4eaa6be4e282085e09d11b60bc182
SavaglyAching <154733755+SavaglyAching@users.noreply.github.com>
2025-03-12 15:29:17 -0300

    Update and rename configuration.nix to default.nix

---
+++ hosts/nixos-mini/default.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+     # System modules
+    ../../modules/system/boot.nix
+    ../../modules/system/network.nix
+    ../../modules/system/users.nix
+    ../../modules/system/shell.nix
+    ../../modules/system/packages.nix
+    ../../modules/system/nix.nix
+
+    # Desktop environment (choose one)
+    ../../modules/desktop/kde.nix
+    
+    # Services (uncomment as needed)
+    ../../modules/services/ssh.nix
+    ../../modules/services/tailscale.nix
+    # ../../modules/services/ollama.nix
+    ../../modules/services/docker.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-mini";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Moncton";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "nixos-unstable";
+}
