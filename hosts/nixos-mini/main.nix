26aa20785992218e4ad593628d1e81a3263f7e83
SavaglyAching <154733755+SavaglyAching@users.noreply.github.com>
2025-03-12 15:02:04 -0300

    Deleted packages

--- hosts/nixos-mini/main.nix
+++ hosts/nixos-mini/main.nix
@@ -2,43 +2,6 @@
 
 {
   
-  environment.systemPackages = with pkgs; [
-    alacritty
-    btop
-    btrfs-progs
-    curl
-    docker-compose
-    ffmpeg-full
-    frigate
-    gallery-dl
-    git
-    glow
-    gitui
-    rclone-browser
-    htop
-    instaloader
-    temurin-jre-bin-21
-    lazygit
-    lazydocker
-    mc
-    mesa
-    micro
-    mosh
-    neovim
-    nmap
-    intel-gpu-tools
-    rclone
-    spotdl
-    tmux
-    trayscale
-    unzip
-    vscode
-    wget
-    yt-dlp
-    zip
-  ];
-
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   security.rtkit.enable = true;
@@ -48,20 +11,4 @@
   programs.mosh.enable = true;
   programs.nix-ld.enable = true;
 
-    programs.bash = {
-      shellAliases = {
-        nr = "sudo nixos-rebuild switch";
-        nm = "cd /etc/nixos";
-        mm = "sudo micro /etc/nixos/main.nix";
-        m = "sudo micro";
-        lzd = "lazydocker";
-        miniterm = "bash /scripts/terminal.sh";
-      };
-    };
-
-  environment.etc."gitconfig".text = ''
-    [user]
-      name = ham
-      email = git@bloood.ca
-  '';
 }
