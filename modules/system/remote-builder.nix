a73d3e5a510548972a7ff86b468aff4ac4632e63
gitbub <git@bloood.ca>
2025-04-21 18:09:38 -0300

    remote builder

--- modules/system/remote-builder.nix
+++ modules/system/remote-builder.nix
@@ -1,19 +1,23 @@
 { config, lib, pkgs, ... }:
 {
 
-   nix = {
-  distributedBuilds = true;
-  buildMachines = [{
-    hostName = "192.168.2.254";  # Use Tailscale hostname
-    system = "x86_64-linux";
-    maxJobs = 8;
-    speedFactor = 2;
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    mandatoryFeatures = [ ];
-    sshUser = "ham";
-    sshKey = "/home/ham/.ssh/id_ed25519";
-  }];
-};
+   # SSH Configuration for Distributed Builds
+  programs.ssh = {
+    extraConfig = ''
+      Host eu.nixbuild.net
+      PubkeyAcceptedKeyTypes ssh-ed25519
+      ServerAliveInterval 60
+      IPQoS throughput
+      IdentityFile /home/ham/.ssh/my-nixbuild-key
+    '';
+    knownHosts = {
+      nixbuild = {
+        hostNames = [ "eu.nixbuild.net" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+      };
+    };
+  };
+  
 
 
 }
