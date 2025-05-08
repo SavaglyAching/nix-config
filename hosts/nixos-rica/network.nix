4bee0c3d68cb718e0d4d361990e81f73a75c33bb
gitbub <git@bloood.ca>
2025-05-08 04:41:47 -0300

    sdf

--- hosts/nixos-rica/network.nix
+++ hosts/nixos-rica/network.nix
@@ -11,7 +11,7 @@
         prefixLength = 24;
       }];
     };
-    nameservers = [ "9.9.9.9" ];
+    nameservers = [ "1.1.1.1" "8.8.8.8" ]; # Changed to Cloudflare and Google DNS
 
     # Firewall settings
     firewall = {
