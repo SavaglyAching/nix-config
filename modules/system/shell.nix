e8e652e2a2b59035c1e5adf0cbe03c7b74fab99d
gitbub <git@bloood.ca>
2025-05-25 00:52:56 -0300

    yt del

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -1,11 +1,10 @@
-{ config, lib, pkgs, ... }: # This indicates the file is a NixOS module.
+{ config, lib, pkgs, ... }:
 
 {
   # Shell and Terminal Configuration
-  programs = { # Main 'programs' block
-
+  programs = {
     # ZSH Configuration
-    zsh = { # Zsh specific configurations
+    zsh = {
       enable = true;
       autosuggestions.enable = true;
       syntaxHighlighting.enable = true;
@@ -16,8 +15,7 @@
         nrf = "sudo nixos-rebuild switch --flake .#";
         nm = "cd /etc/nixos";
         nixcfg = "cd /home/ham/Documents/nixos-test";
-        
-
+    
         # Editor shortcuts
         m = "sudo micro";
         mm = "sudo micro /home/ham/nixos-config/configuration.nix";
@@ -32,22 +30,8 @@
         
         # Misc
         miniterm = "bash /scripts/terminal.sh";
-      }; # shellAliases block is closed
-
-      # THIS IS CORRECTLY PLACED:
-      initExtra = ''
-        # Your Zsh function goes here, exactly as it would in .zshrc
-        # (These comments below are for Zsh, so they correctly use # already)
-        yt() {
-            yt-dlp --retries infinite --fragment-retries infinite --socket-timeout 60 "$@"
-        }
-
-        # You can add other functions or Zsh commands here too
-        # another_func() {
-        #   echo "Hello from Zsh initExtra"
-        # }
-      ''; # initExtra is defined as a direct attribute of zsh
-    }; # zsh block is closed
+      };
+    };
     
     # TMUX Configuration
     tmux = {
@@ -55,9 +39,9 @@
       plugins = with pkgs; [
         tmuxPlugins.better-mouse-mode
       ];
-    }; # tmux block is closed
+    };
     
     # Mosh Terminal
     mosh.enable = true;
-  }; # programs block is closed
+  };
 }
