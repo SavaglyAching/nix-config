09ad3f23fd8979c4fc65f355cb4851ddec9e2f8d
git <git@bloood.ca>
2025-06-09 18:27:10 -0300

    remived hm and sops from flake

--- flake.nix
+++ flake.nix
@@ -3,47 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    # Add sops-nix input
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, sops-nix, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
     nixosConfigurations = {
       "nixos-desk" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/nixos-desk
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops  # Add sops module
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ham = { config, pkgs, ... }: import ./home/ham.nix {
-              inherit config pkgs;
-            };
-          }
         ];
       };
-      
+
       "nixos-rica" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/nixos-rica
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops  # Add sops module
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ham = import ./home/ham.nix;
-          }
         ];
       };
 
@@ -51,13 +26,6 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/nixos-mini
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops  # Add sops module
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ham = import ./home/ham.nix;
-          }
         ];
       };
 
@@ -66,15 +34,6 @@
         modules = [
           ./hosts/nixos-surface
           nixos-hardware.nixosModules.microsoft-surface-pro-intel
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops  # Add sops module
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ham = { config, pkgs, ... }: import ./home/ham.nix {
-              inherit config pkgs;
-            };
-          }
         ];
       };
 
@@ -82,13 +41,6 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/nixos-vm
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops  # Add sops module
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ham = import ./home/ham.nix;
-          }
         ];
       };
     };
