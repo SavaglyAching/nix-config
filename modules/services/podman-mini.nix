39554deb0fc5b4827bc216eed45a5f3e91dd0235
git <git@bloood.ca>
2025-06-29 08:25:40 -0300

    feat: replace metube with ytptube

--- modules/services/podman-mini.nix
+++ modules/services/podman-mini.nix
@@ -1,34 +1,14 @@
 {
+  virtualisation.podman.enable = true;
 
-
-  virtualisation.oci-containers.containers."metube" = {
-    image = "alextaaga/metube";
+  virtualisation.oci-containers.containers."ytptube" = {
+    image = "ghcr.io/arabcoders/ytptube:latest";
     ports = [ "8081:8081" ];
     volumes = [ "/home/ham/metube-downloads:/downloads" ];
-    dependsOn = [ "ntfy" ];
     environment = {
-      "YTDL_OPTIONS" = builtins.toJSON {
-        retries = "infinite";
-        fragment_retries = "infinite";
-        socket_timeout = 90;
-        no_part = true;
-      };
-      "DOWNLOAD_MODE" = "concurrent";
-      "NTFY_URL" = "http://localhost:6789";
-      "NTFY_TOPIC" = "metube";
+      "YTP_MAX_WORKERS" = "999"; # Effectively unlimited concurrent downloads
+      "YTP_CONSOLE_ENABLED" = "true"; # Enable console
     };
     extraOptions = [ "--restart=unless-stopped" ];
-  };
-  virtualisation.oci-containers.containers."ntfy" = {
-    image = "binwiederhier/ntfy";
-    ports = [ "6789:80" ];
-    volumes = [
-      "/home/ham/ntfy/cache:/var/cache/ntfy"
-      "/home/ham/ntfy/config:/etc/ntfy"
-    ];
-    extraOptions = [
-      "--restart=unless-stopped"
-    ];
-    command = [ "serve" ];
   };
 }
